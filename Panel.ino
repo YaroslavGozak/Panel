9b1152b3c7018dfc145c034256e348a4f280d3bf
Yaroslav Gozak <gozakyaroslav@gmail.com>
2021-03-07 00:31:28 +0200

    Added CHECK button
    Added buzzer

--- Panel.ino
+++ Panel.ino
@@ -1,44 +1,40 @@
-const int LED1_PIN = 12;
-const int LED2_PIN = 5;
-const int LED3_PIN = 14;
-const int LED4_PIN = 4;
+const int LED_PINS[] = {12, 5, 14, 4};
 
 const int RELE_BUTTON_PIN = 13;
-//const int CHECK_BUTTON_PIN = 8;
+const int CHECK_BUTTON_PIN = 15;
+const int BUZZER_PIN = 16;
 
 bool currentReleButtonState;
-//bool currentCheckButtonState;
+bool currentCheckButtonState;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  pinMode(LED3_PIN, OUTPUT);
-  pinMode(LED4_PIN, OUTPUT);
+  for(int i = 0; i < 4; i++){
+    pinMode(LED_PINS[i], OUTPUT);
+  }
 
   pinMode(RELE_BUTTON_PIN, INPUT_PULLUP);
-   //pinMode(CHECK_BUTTON_PIN, INPUT);
+  pinMode(CHECK_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
 
   currentReleButtonState = digitalRead(RELE_BUTTON_PIN);
-   //currentCheckButtonState = digitalRead(CHECK_BUTTON_PIN);
-  if(currentReleButtonState){
-    led2On();
-  }
+  currentCheckButtonState = digitalRead(CHECK_BUTTON_PIN);
+
   Serial.begin(9600);
   Serial.write("Setup complete\n");
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  if(isCheckButtonPressedUp()){
+    toggleLed(LED_PINS[0]);
+    makeSound();
+  }
   if(isReleButtonPressedUp()){
-    led1On();
+    toggleLed(LED_PINS[1]);
+    makeSound();
   }
-  else{
-    led1Off();
-  }
-  blinkAll();
 }
 
 bool isReleButtonPressedUp(){
@@ -49,58 +45,45 @@ bool isReleButtonPressedUp(){
   return currentReleButtonState && !lastReleButtonState;
 }
 
-/*bool isCheckButtonPressedUp(){
-  bool newCheckButtonState = digitalRead(RELE_BUTTON_PIN);
+bool isCheckButtonPressedUp(){
+  bool newCheckButtonState = digitalRead(CHECK_BUTTON_PIN);
+  Serial.write("newCheckButtonState "); Serial.print(newCheckButtonState); Serial.write("\n");
   bool lastCheckButtonState = currentCheckButtonState;
   currentCheckButtonState = newCheckButtonState;
   return !currentCheckButtonState && lastCheckButtonState;
-}*/
+}
 void blinkAll(){
-  //led1On();
-  //led2On();
-  led3On();
-  led4On();
+  ledOn(LED_PINS[2]);
+  ledOn(LED_PINS[3]);
 
   delay(500);
-
-  //led1Off();
-  //led2Off();
-  led3Off();
-  led4Off();
+  
+  ledOff(LED_PINS[2]);
+  ledOff(LED_PINS[3]);
 
   delay(500);
 }
 
-void led1On(){
-  digitalWrite(LED1_PIN, HIGH);
-  //Serial.println("LED 1 on");
+void ledOn(int pin){
+  digitalWrite(pin, HIGH);
 }
 
-void led2On(){
-  digitalWrite(LED2_PIN, HIGH);
+void ledOff(int pin){
+  digitalWrite(pin, LOW);
 }
 
-void led3On(){
-  digitalWrite(LED3_PIN, HIGH);
+void toggleLed(int pin){
+  int state = digitalRead(pin);
+  if(state){
+    ledOff(pin);
+  }
+  else{
+    ledOn(pin);
+  }
 }
 
-void led4On(){
-  digitalWrite(LED4_PIN, HIGH);
-}
-
-void led1Off(){
-  digitalWrite(LED1_PIN, LOW);
-  //Serial.println("LED 1 off");
-}
-
-void led2Off(){
-  digitalWrite(LED2_PIN, LOW);
-}
-
-void led3Off(){
-  digitalWrite(LED3_PIN, LOW);
-}
-
-void led4Off(){
-  digitalWrite(LED4_PIN, LOW);
+void makeSound(){
+  digitalWrite(BUZZER_PIN ,HIGH);
+  delay(100);
+  digitalWrite(BUZZER_PIN, LOW);
 }
